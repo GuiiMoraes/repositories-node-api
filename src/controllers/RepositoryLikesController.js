c5f22178d8db42be69c671a428583356624162ce
Guilherme Moraes <guii.moraes.93@gmail.com>
2020-04-29 16:04:54 -0300

    feat(RepositoriesController): create store of repositories like

--- src/controllers/RepositoryLikesController.js
+++ src/controllers/RepositoryLikesController.js
@@ -1,10 +1,33 @@
-const { uuid } = require('uuidv4');
+const { isUuid } = require('uuidv4');
 
 const repositoriesDatabase = require('../database/Repositories');
 
 class RepositoryLikesController {
   store(req, res) {
-    console.log('RepositoryLikesController.store');
+
+    const { id } = req.params;
+
+    if (!isUuid(id)) {
+      return res.status(400).json({ error: 'this is not an valid id' });
+    }
+
+    const repositoryIndex = repositoriesDatabase.findIndex(
+      (repo) => repo.id === id
+    );
+
+    if (repositoryIndex < 0) {
+      return res.status(400).json({ error: 'repository not founded' });
+    }
+
+    repositoriesDatabase[repositoryIndex] = {
+      id,
+      title: repositoriesDatabase[repositoryIndex].title,
+      url: repositoriesDatabase[repositoryIndex].url,
+      techs: repositoriesDatabase[repositoryIndex].techs,
+      likes: repositoriesDatabase[repositoryIndex].likes + 1,
+    };
+
+    return res.json(repositoriesDatabase);
   }
 }
 
