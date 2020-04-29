a096ce391b67b35330500e466480a227e192cfe1
Guilherme Moraes <guii.moraes.93@gmail.com>
2020-04-29 15:51:50 -0300

    feat(RepositoriesController): create update of repositories

--- src/controllers/RepositoriesController.js
+++ src/controllers/RepositoriesController.js
@@ -1,10 +1,10 @@
-const { uuid } = require('uuidv4');
+const { uuid, isUuid } = require('uuidv4');
 
 const repositoriesDatabase = require('../database/Repositories');
 
 class RepositoriesController {
   show(req, res) {
-    return res.json({'RepositoriesController.show': repositoriesDatabase});
+    return res.json({ 'RepositoriesController.show': repositoriesDatabase });
   }
 
   store(req, res) {
@@ -15,14 +15,37 @@ class RepositoriesController {
       title,
       url,
       techs,
-      likes: 0
+      likes: 0,
     });
 
-    return res.json({repositoriesDatabase});
+    return res.json({ repositoriesDatabase });
   }
 
   update(req, res) {
-    console.log('RepositoriesController.update');
+    const { id } = req.params;
+    const { title, url, techs } = req.body;
+
+    if (!isUuid(id)) {
+      return res.status(400).json({ error: 'this is not an valid id' });
+    }
+
+    let repositoryIndex = repositoriesDatabase.findIndex(
+      (repo) => repo.id === id
+    );
+
+    if (repositoryIndex > 0) {
+      return res.status(400).json({ error: 'repository not founded' });
+    }
+
+    repositoriesDatabase[repositoryIndex] = {
+      id,
+      title,
+      url,
+      techs,
+      likes: repositoriesDatabase[repositoryIndex].likes,
+    };
+
+    return res.json(repositoriesDatabase);
   }
 
   delete(req, res) {
